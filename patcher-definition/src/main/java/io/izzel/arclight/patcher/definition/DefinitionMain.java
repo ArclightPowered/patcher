98374901b981eee0912628d3336d6f9532f8b346
Joseph Tarbit <joetarbit@gmail.com>
2022-05-17 15:10:02 +0100

    Update libs & refactoring

--- patcher-definition/src/main/java/io/izzel/arclight/patcher/definition/DefinitionMain.java
+++ patcher-definition/src/main/java/io/izzel/arclight/patcher/definition/DefinitionMain.java
@@ -1,33 +1,28 @@
 package io.izzel.arclight.patcher.definition;
 
-import io.izzel.arclight.api.PluginPatcher;
-import org.objectweb.asm.tree.ClassNode;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
+import org.objectweb.asm.tree.ClassNode;
+
+import io.izzel.arclight.api.PluginPatcher;
+
 public class DefinitionMain implements PluginPatcher {
 
-    private static final Map<String, BiConsumer<ClassNode, ClassRepo>> SPECIFIC = new HashMap<String, BiConsumer<ClassNode, ClassRepo>>() {};
-    private static final List<BiConsumer<ClassNode, ClassRepo>> GENERAL = new ArrayList<>();
-
-    static {
-        SPECIFIC.put("com/sk89q/worldedit/bukkit/BukkitAdapter", WorldEdit::handleBukkitAdapter);
-        SPECIFIC.put("com/sk89q/worldedit/bukkit/adapter/Refraction", WorldEdit::handlePickName);
-        GENERAL.add(WorldEdit::handleGetProperties);
-        GENERAL.add(WorldEdit::handleWatchdog);
-    }
+    private static final Map<String, BiConsumer<ClassNode, ClassRepo>> SPECIFIC = Map.of(
+            "com/sk89q/worldedit/bukkit/BukkitAdapter", WorldEdit::handleBukkitAdapter,
+            "com/sk89q/worldedit/bukkit/adapter/Refraction", WorldEdit::handlePickName);
+    private static final List<BiConsumer<ClassNode, ClassRepo>> GENERAL = List.of(
+            WorldEdit::handleGetProperties, WorldEdit::handleWatchdog);
 
     @Override
     public void handleClass(ClassNode node, ClassRepo classRepo) {
-        BiConsumer<ClassNode, ClassRepo> consumer = SPECIFIC.get(node.name);
+        var consumer = SPECIFIC.get(node.name);
         if (consumer != null) {
             consumer.accept(node, classRepo);
         } else {
-            for (BiConsumer<ClassNode, ClassRepo> general : GENERAL) {
+            for (var general : GENERAL) {
                 general.accept(node, classRepo);
             }
         }
